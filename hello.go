0a3af2c7193ac4fb95a5fd2601ea368292ff9547
gTerrusa <gterrusa743@gmail.com>
2021-02-13 11:39:54 -0800

    add default value to Hello

--- hello.go
+++ hello.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 )
 
+// Constants should improve performance of your application
+// as it saves you creating the "Hello, " string instance
+// every time Hello is called.
+const englishHelloPrefix = "Hello, "
+
 // Hello accepts a string, an returns the string "Hello, " + name
+// defaults to return "Hello, World" if empty string given
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World"))
 }
