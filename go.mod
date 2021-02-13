d2335eeca1fea2f977fc294a9b24086a953896e5
gTerrusa <gterrusa743@gmail.com>
2021-02-13 11:17:32 -0800

    initial commit

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/gTerrusa/hello
 
 go 1.15
+
+require (
+	golang.org/x/tools v0.1.0 // indirect
+	rsc.io/quote v1.5.2
+)
--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Hello accepts a string, an returns the string "Hello, " + name
+func Hello(name string) string {
+	return "Hello, " + name
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(Hello("world"))
 }
